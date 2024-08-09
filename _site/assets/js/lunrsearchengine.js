970c991b7a878054a3c433fe07cd885b2896c2e1
Jake Scally <83603308+jakescally@users.noreply.github.com>
2024-08-09 00:04:13 -0400

    update jumbotron

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -1,52 +1,52 @@
 
 var documents = [{
     "id": 0,
-    "url": "http://localhost:4000/home/404.html",
+    "url": "https://jakescally.github.io/404.html",
     "title": "404",
     "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "http://localhost:4000/home/about",
+    "url": "https://jakescally.github.io/about",
     "title": "About Scallywag Network",
     "body": "Hey! This is my website. I'm Jake Scally, a Physics and Astrophysics student at FSU. I'm into astrophotography, 3D printing, 3D modeling, app development, piano, retro video games and technology, CRT TV repair, and more.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Etc. Thanks for stopping by! "
     }, {
     "id": 2,
-    "url": "http://localhost:4000/home/categories",
+    "url": "https://jakescally.github.io/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 3,
-    "url": "http://localhost:4000/home/gallery",
+    "url": "https://jakescally.github.io/gallery",
     "title": "Gallery",
     "body": ""
     }, {
     "id": 4,
-    "url": "http://localhost:4000/home/",
+    "url": "https://jakescally.github.io/",
     "title": "Home",
     "body": "      Featured:                                                                                                                                                                                                                 Imaging the Orion Nebula                              :               Here is a shot of the Orion Nebula I got in Tallahassee. I’ll explain more in-detail at a later date, as I don’t quite have. . . :                                                                                                                                                                               Jake                                    25 Jan 2024                                                                                                                                                                                                                                                                                                                                                            Imaging Andromeda for the First Time                              :               Don’t have the time to fill this out fully, but here is my stacked image of Andromeda from my backyard. :                                                                                                                                                                               Jake                                    25 Jan 2024                                                                                                                                                                      All Stories:                                                                                                     Some Moon Shots              :       Some moon pictures. I’ll put more detail here later when I have time. :                                                                               Jake                25 Jan 2024                                                                                                                                     Imaging the Orion Nebula              :       Here is a shot of the Orion Nebula I got in Tallahassee. I’ll explain more in-detail at a later date, as I don’t quite have the time right now. :                                                                               Jake                25 Jan 2024                                                                                                                                     Imaging Andromeda for the First Time              :       Don’t have the time to fill this out fully, but here is my stacked image of Andromeda from my backyard. :                                                                               Jake                25 Jan 2024                                                                                                                                     The CRT Stash As Of 2024              :       Hey! I collect, repair, service, and modify old CRT televisions and monitors!:                                                                               Jake                23 Jan 2024                                            "
     }, {
     "id": 5,
-    "url": "http://localhost:4000/home/robots.txt",
+    "url": "https://jakescally.github.io/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 6,
-    "url": "http://localhost:4000/home/some-moon-shots/",
+    "url": "https://jakescally.github.io/some-moon-shots/",
     "title": "Some Moon Shots",
     "body": "2024/01/25 - Some moon pictures. I’ll put more detail here later when I have time. "
     }, {
     "id": 7,
-    "url": "http://localhost:4000/home/imaging-the-orion-nebula/",
+    "url": "https://jakescally.github.io/imaging-the-orion-nebula/",
     "title": "Imaging the Orion Nebula",
     "body": "2024/01/25 - Here is a shot of the Orion Nebula I got in Tallahassee. I’ll explain more in-detail at a later date, as I don’t quite have the time right now. "
     }, {
     "id": 8,
-    "url": "http://localhost:4000/home/astro-images-thus-far/",
+    "url": "https://jakescally.github.io/astro-images-thus-far/",
     "title": "Imaging Andromeda for the First Time",
     "body": "2024/01/25 - Don’t have the time to fill this out fully, but here is my stacked image of Andromeda from my backyard. "
     }, {
     "id": 9,
-    "url": "http://localhost:4000/home/my-crt-history/",
+    "url": "https://jakescally.github.io/my-crt-history/",
     "title": "The CRT Stash As Of 2024",
     "body": "2024/01/23 - Hey! I collect, repair, service, and modify old CRT televisions and monitors! I’m super busy right now, so I haven’t had the time to fill in this page, but eventually I’ll list here how I got into the hobby, my first few TVs, and more. "
     }];
